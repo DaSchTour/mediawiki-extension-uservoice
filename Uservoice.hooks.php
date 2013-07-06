774f21b837c1931a7edca2b27c9c7c2b00cb2ea6
DaSch <dasch@daschmedia.de>
2013-07-06 19:07:29 +0200

    missed to read global

--- Uservoice.hooks.php
+++ Uservoice.hooks.php
@@ -23,6 +23,7 @@ class UservoiceHooks {
        return true;
 	}
     public static function addUservoice () {
+        global $wgUservoiceKey, $wgUservoiceMode, $wgUservoicePrimColor, $wgUservoiceLinkColor, $wgUservoiceDefaultMode, $wgUservoiceID, $wgUservoiceLabel, $wgUservoiceTabColor, $wgUservoicePosition;
         $html = <<<USERVOICE
     <!-- UserVoice JavaScript SDK (only needed once on a page) -->
 <script>(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/{$wgUservoiceKey}.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()</script>
