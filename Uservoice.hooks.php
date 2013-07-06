7a112345d4d58b46b50cb16b85daa10f31e7ed21
DaSch <dasch@daschmedia.de>
2013-07-06 18:48:36 +0200

    new extension

---
+++ Uservoice.hooks.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Javascript Slideshow
+ * Javascript Slideshow Hooks
+ *
+ * @author 		@See $wgExtensionCredits
+ * @license		GPL
+ * @package		FormatNum
+ * @addtogroup  Extensions
+ * @link		http://www.mediawiki.org/wiki/Extension:FormatNum
+ *
+ **/
+
+class UservoiceHooks {
+	/**
+	 * Setup parser function
+	 * @param parser from mediawiki
+	 * @return must return true for next prasers
+	 **/
+	public static function setupUservoice( OutputPage &$out, Skin &$skin ) {
+	   $script = UservoiceHooks::addUservoice();
+       $out->addScript( $script );
+       return true;
+	}
+    public static function addUservoice () {
+        $html = <<<USERVOICE
+    <!-- UserVoice JavaScript SDK (only needed once on a page) -->
+<script>(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/{$wgUservoiceKey}.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()</script>
+
+<!-- A tab to launch the Classic Widget -->
+<script>
+UserVoice = window.UserVoice || [];
+UserVoice.push(['showTab', 'classic_widget', {
+  mode: '{$wgUservoiceMode}',
+  primary_color: '{$wgUservoicePrimColor}',
+  link_color: '{$wgUservoiceLinkColor}',
+  default_mode: '{$wgUservoiceDefaultMode}',
+  forum_id: {$wgUservoiceID},
+  tab_label: '{$wgUservoiceLabel}',
+  tab_color: '{$wgUservoiceTabColor}',
+  tab_position: '{$wgUservoicePosition}',
+  tab_inverted: false
+}]);
+</script>
+USERVOICE;
+        return $html;
+    }
+}
---
+++ Uservoice.php
@@ -0,0 +1,34 @@
+<?php
+/** 
+* @addtogroup Extensions 
+*/
+// Check environment
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo( "This is an extension to the MediaWiki package and cannot be run standalone.\n" );
+	die( -1 );
+}
+
+/* Configuration */
+
+// Credits
+$wgExtensionCredits['other'][] = array (
+	'path'=> __FILE__ ,
+	'name'=>'Uservoice',
+	'url'=>'https://github.com/DaSchTour/mediawiki-extension-uservoice',
+	'description'=>'Adding uservoice.',
+//	'descriptionmsg' => 'uservoice-desc',
+	'author'=>'[http://www.daschmedia.de DaSch]',
+	'version'=>'0.9',
+);
+
+$dir = dirname( __FILE__ ) . '/';
+
+//$wgExtensionMessagesFiles['Uservoice']      = $dir . 'ExtensionVersion.i18n.php';
+//$wgExtensionMessagesFiles['UservoiceAlias'] = $dir . 'ExtensionVersion.alias.php';
+
+$wgAutoloadClasses[ 'UservoiceHooks' ]    = $dir . 'Uservoice.hooks.php';
+$wgHooks['BeforePageDisplay'][] = 'UservoiceHooks::setupUservoice';
+
+$wgUservoiceMode = 'full';
+$wgUservoiceDefaultMode = 'feedback';
+$wgUservoicePosition = 'middle-right';
